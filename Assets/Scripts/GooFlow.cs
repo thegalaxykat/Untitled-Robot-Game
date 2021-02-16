880da4ab8d047d4dbb51a0263901956016a8ee4e
Kat Canavan <Katcanavan@gmail.com>
2021-02-16 14:48:07 -0500

    Goo flow length limited (noticed jumping broken)

--- Assets/Scripts/GooFlow.cs
+++ Assets/Scripts/GooFlow.cs
@@ -6,31 +6,32 @@ public class GooFlow : MonoBehaviour
 {// this script attatched to the goo itself
 
     public float stretchFactor; // where 1 is a whole tile
-    
+    public float LimitFlowDist; // where 1 = 1 tile
+
+    private float distTraveled = 1f; //includes initial length
+
     // Start is called before the first frame update
     void Start()
     {
 
-        //*Flow();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        GetComponent<SpriteRenderer>().size += new Vector2(stretchFactor, 0); //(test)
+    {       
+       Flow();
+        ////GetComponent<SpriteRenderer>().size += new Vector2(stretchFactor, 0); //(test)
     }
 
  void Flow() //goo spreads out on level ground (if it hits a ledge or a change in height it just stops)
     {
-        //sprite width increased by .25 each frame 12 times (that should be 3 tiles)
-        
-        //every time this line is run the sprite will increase by the stretch factor
-        GetComponent<SpriteRenderer>().size += new Vector2(stretchFactor, 0);
-
-
-        //! note: to change flow direction, flip sprite
-
-
+		if (distTraveled < LimitFlowDist)
+		{
+			//every time this line is run the sprite will increase by the stretch factor
+			GetComponent<SpriteRenderer>().size += new Vector2(stretchFactor, 0);
+			//add the distance moved to distTravled
+			distTraveled += stretchFactor;
+        }
     }
     
 }

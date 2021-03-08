e46b4b93fb4c634803cb26b9ded85c63c0acaa9e
Kat Canavan <Katcanavan@gmail.com>
2021-03-08 16:10:22 -0500

    Level 1 Finished

---
+++ Assets/Scripts/AppearOnButtonPress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearOnButtonPress : MonoBehaviour{
+
+    public GameObject Canvas;
+    private bool buttonActivated;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<ButtonCollison>().buttonActivated == true)
+        {
+            Canvas.SetActive(true);
+            print ("pressed");
+        }
+        else{
+            Canvas.SetActive(false);
+        }
+    }
+}

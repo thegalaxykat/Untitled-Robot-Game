9f123aeb9079fbe1398fd1890e20cf74b0f9040e
Kat Canavan <Katcanavan@gmail.com>
2021-03-11 12:56:08 -0500

    Title Screen

---
+++ Assets/UI/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+        public void Level1() {  
+        SceneManager.LoadScene("Level 1");
+    }  
+
+	public void exitgame()
+	{
+		Debug.Log("exitgame");
+		Application.Quit();
+	}
+
+}

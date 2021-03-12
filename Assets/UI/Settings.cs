6d4cce0a92bb9fb88574170cf2f1ef46bd86e26d
Kat Canavan <Katcanavan@gmail.com>
2021-03-12 15:58:01 -0500

    settings and credits pages

---
+++ Assets/UI/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Settings : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        Debug.Log("Main Menu");
+        SceneManager.LoadScene("Title Screen");
+    }
+}
